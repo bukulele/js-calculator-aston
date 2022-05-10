f10becae8369daea503923d6b3f560502af355b4
nukulele <zonough@gmail.com>
2022-05-10 17:37:26 +0400

    calculator is ready

--- js/calc.js
+++ js/calc.js
@@ -1,3 +1,5 @@
+window.addEventListener("keydown", keyboardListener);
+
 const calculatorButtons = document.getElementById("calculator-buttons");
 calculatorButtons.addEventListener("click", handleButtonClick);
 
@@ -9,12 +11,54 @@ const maxUserInputLength = 21;
 
 let calcDone = false;
 
+const calcElements = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "clear",
+  "delete",
+  "%",
+  "÷",
+  "x",
+  "-",
+  "+",
+  "equal",
+  ".",
+];
+
+function executeClick(data) {
+  if (data === "equal" && calcDone === false) {
+    calcResult(formulaWindow.innerText, data);
+  } else if (data === "%") {
+    calcResult(formulaWindow.innerText, data);
+  } else if (data !== "equal" && data !== "%") {
+    if (data === "clear" || (calcDone === true && data === "delete")) {
+      formulaWindow.innerText = "0";
+      userInputWindow.innerText = "0";
+      calcDone = false;
+    } else if (
+      (formulaWindow.innerText.length < maxFormulaLength &&
+        userInputWindow.innerText.length < maxUserInputLength &&
+        data) ||
+      data === "delete"
+    ) {
+      manageUserInputWindow(data);
+      manageFormulaWindow(data);
+    }
+  }
+}
+
 function handleButtonClick(event) {
   let data;
   if (event.target.classList.contains("button-operation")) {
-    if (event.target.id === "equal" && calcDone === false) {
-      calcResult(formulaWindow.innerText);
-    } else if (event.target.id === "division") {
+    if (event.target.id === "division") {
       data = "÷";
     } else if (event.target.id === "multiply") {
       data = "x";
@@ -22,24 +66,47 @@ function handleButtonClick(event) {
       data = "-";
     } else if (event.target.id === "plus") {
       data = "+";
-    } else if (event.target.id !== "equal") {
+    } else if (event.target.id === "percentage") {
+      data = "%";
+    } else {
       data = event.target.id;
     }
   } else if (event.target.classList.contains("button-number")) {
     data = event.target.innerText;
   }
-  if (data === "clear" || (calcDone === true && data === "delete")) {
-    formulaWindow.innerText = "0";
-    userInputWindow.innerText = "0";
-    calcDone = false;
-  } else if (
-    (formulaWindow.innerText.length < maxFormulaLength &&
-      userInputWindow.innerText.length < maxUserInputLength &&
-      data) ||
-    data === "delete"
-  ) {
-    manageUserInputWindow(data);
-    manageFormulaWindow(data);
+  executeClick(data);
+}
+
+function keyboardListener(event) {
+  let code = event.code;
+  let shift = event.shiftKey;
+  if (code.includes("Digit")) {
+    code = code.slice(5);
+  } else if (code.includes("Numpad")) {
+    code = code.slice(6);
+  }
+  if (code === "Minus" || code === "Subtract") {
+    code = "-";
+  } else if (code === "Add" || (code === "Equal" && shift)) {
+    code = "+";
+  } else if (code === "Multiply" || (code === "8" && shift)) {
+    code = "x";
+  } else if (code === "5" && shift) {
+    code = "%";
+  } else if (code === "Decimal" || code === "Period") {
+    code = ".";
+  } else if (code === "Equal" || code === "Enter") {
+    code = "equal";
+  } else if (code === "Slash" || code === "Divide") {
+    event.preventDefault();
+    code = "÷";
+  } else if (code === "Escape" || code === "Delete") {
+    code = "clear";
+  } else if (code === "Backspace") {
+    code = "delete";
+  }
+  if (calcElements.includes(code)) {
+    executeClick(code);
   }
 }
 
@@ -179,16 +246,39 @@ function manageFormulaWindow(data) {
   }
 }
 
-function calcResult(expression) {
+function calcResult(expression, operation) {
   const exp = convertExpression(expression);
-  let calc = new Function("return " + exp);
-  let result = (calc().toFixed(9) * 100) / 100;
-  let i = formulaWindow.innerText.length - 1;
-  while (isNaN(formulaWindow.innerText[i])) {
-    i--;
+  let result;
+  if (operation === "equal") {
+    let calc = new Function("return " + exp.join(""));
+    result = (calc().toFixed(9) * 100) / 100;
+    let i = formulaWindow.innerText.length - 1;
+    while (isNaN(formulaWindow.innerText[i])) {
+      i--;
+    }
+    let newFormula = formulaWindow.innerText.slice(0, i + 1);
+    formulaWindow.innerText = newFormula + "=";
+  } else if (operation === "%") {
+    let slicedExp = exp.slice(0, exp.length - 2);
+    let percentValue = exp[exp.length - 1];
+    let preCalcFunction = new Function("return " + slicedExp.join(""));
+    let preCalcResult = (preCalcFunction().toFixed(9) * 100) / 100;
+    if (exp[exp.length - 2] === "-") {
+      result = preCalcResult - (preCalcResult * percentValue) / 100;
+    } else if (exp[exp.length - 2] === "+") {
+      result = preCalcResult + (preCalcResult * percentValue) / 100;
+    } else if (exp[exp.length - 2] === "*") {
+      result = (preCalcResult * percentValue) / 100;
+    } else if (exp[exp.length - 2] === "/") {
+      result = (preCalcResult / percentValue) * 100;
+    }
+    let i = formulaWindow.innerText.length - 1;
+    while (isNaN(formulaWindow.innerText[i])) {
+      i--;
+    }
+    let newFormula = formulaWindow.innerText.slice(0, i + 1);
+    formulaWindow.innerText = newFormula + "%=";
   }
-  let newFormula = formulaWindow.innerText.slice(0, i + 1);
-  formulaWindow.innerText = newFormula + "=";
   formulaWindow.insertAdjacentText("beforeend", result);
   userInputWindow.innerText = result;
   calcDone = true;
@@ -208,5 +298,5 @@ function convertExpression(expression) {
   while (isNaN(correctSignsArr[correctSignsArr.length - 1])) {
     correctSignsArr.pop();
   }
-  return correctSignsArr.join("");
+  return correctSignsArr;
 }
